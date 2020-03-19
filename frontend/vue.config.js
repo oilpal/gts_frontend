5197db7f01645487ff1dcc686f4e145ffa207a8c
judongChoi <jd@relaybrand.com>
2020-03-19 18:44:26 +0900

    vue config port 설정 추가

---
+++ frontend/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {  
+    outputDir: "../src/main/resources/static",  
+    indexPath: "../static/index.html",  
+    devServer: {  
+      port:"8300",
+      proxy: "http://localhost:8282"  
+    },  
+    chainWebpack: config => {  
+      const svgRule = config.module.rule("svg");    
+      svgRule.uses.clear();    
+      svgRule.use("vue-svg-loader").loader("vue-svg-loader");  
+    }  
+  };
