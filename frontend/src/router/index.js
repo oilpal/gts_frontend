597458e2912a7fe756a741033722c68252964bb9
judongChoi <jd@relaybrand.com>
2020-03-25 11:24:41 +0900

    [공통] 로그인 Path 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,19 +2,26 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Login from '../views/login/login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
   {
     path: '/',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: '/login',
+    name: 'Login',
+    component: Login
+    // redirect: { name: Login }
   }
 ]
 
